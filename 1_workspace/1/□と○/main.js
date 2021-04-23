de69bc5fabf9acbef172f54df92ae38ce53fe5db
niiakira <niiakira@icloud.com>
2021-04-23 14:20:29 +0900

    [Trello]を学習しました

--- 1_workspace/1/□と○/main.js
+++ 1_workspace/1/□と○/main.js
@@ -1,5 +1,12 @@
 const figure = document.getElementById("figure")
+console.log(figure.className)
+
 
 figure.onclick = function(){
-    figure.classList.toggle(`rounded`)
+    if (figure.className === "transition square")
+    {figure.className = "transition rounded"}
+        else if  (figure.className === "transition rounded")
+        {figure.className = "transition triangle"}
+        else if (figure.className === "transition triangle")
+        {figure.className = "transition square"}
 }
