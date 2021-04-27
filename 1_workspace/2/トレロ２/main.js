7fbc2f42d3d1ceb6cb4dc7153e1aabbd138bc272
niiakira <niiakira@icloud.com>
2021-04-27 11:53:45 +0900

    [trello]のすっきりした文章を学んだ

--- 1_workspace/2/トレロ２/main.js
+++ 1_workspace/2/トレロ２/main.js
@@ -3,22 +3,23 @@ const container = document.getElementById("cards-container")
 const addButton = document.getElementById("add-button")
 
 addButton.onclick = function(){
-    const text = inputElement.value
+    const card = createCard(inputElement.value)
+    container.append(card)
+    inputElement.value = ""
+}
+
+const createCard = function(text){
     const card = document.createElement("div")
     card.className = "card"
     const toDo = document.createElement("div")
-    toDo.className = "todo"
+    toDo.classname = "todo"
     toDo.textContent = text
     card.append(toDo)
-
     const deleteButton = document.createElement("div")
     deleteButton.className = "delete"
     deleteButton.onclick = function(){
         card.remove()
     }
     card.append(deleteButton)
-    
-    container.append(card)
-
-    inputElement.value = ""
+    return card
 }
